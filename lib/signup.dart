bd66ef982ca00ea9f7de643884470ae72f24e00c
Erica Song <ericasong@Ericas-MacBook-Pro.local>
2020-09-19 15:31:34 -0400

    separated students and teachers on sign up form.

--- lib/signup.dart
+++ lib/signup.dart
@@ -9,8 +9,6 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUpPage> {
-  final _firstNameController = TextEditingController();
-  final _lastNameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
   final _schoolController = TextEditingController();
@@ -23,6 +21,68 @@ class _SignUpState extends State<SignUpPage> {
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/earth.png'), //display custom logo
+                SizedBox(height: 16.0), //spacing for aesthetics
+                Text('Create a new account'),
+              ],
+            ),
+
+            SizedBox(height: 120.0), //spacing for aesthetics
+
+            RaisedButton(
+              color: Colors.blue,
+              child: Text(
+              "Teacher",
+              style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Teacher()),
+                );
+              }
+            ),
+
+            RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  "Gap-Year Student",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Student()),
+                  );
+                }
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Teacher extends StatefulWidget {
+  @override
+  _TeacherState createState() => _TeacherState();
+}
+
+class _TeacherState extends State<Teacher> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold( // creates Scaffold
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget> [
             SizedBox(height: 80.0),
             Column(
               children: <Widget>[
@@ -62,7 +122,7 @@ class _SignUpState extends State<SignUpPage> {
             ButtonBar(
               children: <Widget>[
                 FlatButton(
-                  child: Text("Cancel"),
+                  child: Text("Clear"),
                   onPressed: () {
                     _firstNameController.clear();
                     _lastNameController.clear();
@@ -77,25 +137,120 @@ class _SignUpState extends State<SignUpPage> {
                   onPressed: () {
                     if (_firstNameController.text.length != 0 && //TODO: implement auth
                         _lastNameController.text.length != 0) {
-                      Navigator.pushNamed(context, '/home');
+                      Navigator.pushNamed(context, '/home'
+                      );
                     }
                   },
                 )
               ],
             ),
             RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Go Back",
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              }
+                color: Colors.blue,
+                child: Text(
+                  "Go Back",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                }
             )
-          ],
-        ),
-      ),
+          ]
+        )
+      )
     );
   }
 }
+
+class Student extends StatefulWidget {
+  @override
+  _StudentState createState() => _StudentState();
+}
+
+class _StudentState extends State<Student> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  @override
+  Widget build(BuildContext context){
+    return Scaffold( // creates Scaffold
+        body: SafeArea(
+            child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                children: <Widget> [
+                  SizedBox(height: 80.0),
+                  Column(
+                    children: <Widget>[
+                      Image.asset('assets/earth.png'), //display custom logo
+                      SizedBox(height: 16.0), //spacing for aesthetics
+                      Text('Create a new account'),
+                    ],
+                  ),
+                  SizedBox(height: 120.0), //spacing for aesthetics
+                  TextField(
+                    //add a controller to authenticate and clear text
+                    controller: _firstNameController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: "First Name:",
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 12.0,
+                  ),
+
+                  TextField(
+                    controller: _lastNameController,
+                    //add a controller to authenticate and clear text
+                    obscureText: true, //make sure password isn't visible
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      labelText: "Last Name:",
+                    ),
+                  ),
+
+                  SizedBox(height: 24.0),
+
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text("Clear"),
+                        onPressed: () {
+                          _firstNameController.clear();
+                          _lastNameController.clear();
+                        },
+                      ),
+                      RaisedButton(
+                        color: Colors.blue,
+                        child: Text(
+                          "Next",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          if (_firstNameController.text.length != 0 && //TODO: implement auth
+                              _lastNameController.text.length != 0) {
+                            Navigator.pushNamed(context, '/home'
+                            );
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                  RaisedButton(
+                      color: Colors.blue,
+                      child: Text(
+                        "Go Back",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }
+                  )
+                ]
+            )
+        )
+    );
+  }
+}
+
