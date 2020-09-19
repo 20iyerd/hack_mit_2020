75dbe5e17d5fa42a7a5d33ea16b444913a8fbc86
Melanie <watermelanie7@gmail.com>
2020-09-19 12:06:12 -0400

    i need to pull

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -25,12 +25,6 @@ class _BackdropState extends State<Backdrop> //added backdrop for added function
       elevation: 0.0,
       titleSpacing: 0.0,
       backgroundColor: Colors.blueGrey,
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        onPressed: () {
-          Navigator.of(context).pushNamed('/menu'); //navigate to menu
-        },
-      ),
       title: Text('Explore'),
       actions: <Widget>[
         IconButton(
@@ -57,7 +51,51 @@ class _BackdropState extends State<Backdrop> //added backdrop for added function
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
       body: _buildStack(),
-    );
+      drawer: Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+      // Important: Remove any padding from the ListView.
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        DrawerHeader(
+          child: Text('Menu'),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+        ListTile(
+          title: Text('Dashboard'),
+          onTap: () {
+            // Update the state of the app
+            // ...
+            // Then close the drawer
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: Text('Other Matches'),
+          onTap: () {
+            // Update the state of the app
+            // ...
+            // Then close the drawer
+            Navigator.pop(context);
+          },
+        ),
+      ListTile(
+        title: Text('Logout'),
+        onTap: () {
+          // Update the state of the app
+          // ...
+          // Then close the drawer
+          Navigator.pop(context);
+        },
+       ),
+      ],
+    ),
+   ),
+  );
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,3 @@ import 'package:flutter/material.dart';
 
 import 'package:hack_mit_2020/app.dart';
 void main() => runApp(MyApp()); //class which runs the app
-
-//hi
