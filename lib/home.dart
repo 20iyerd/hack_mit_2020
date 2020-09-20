f8db7bb92a0108bfa3d69b2d35681bb830ebaad3
Melanie <watermelanie7@gmail.com>
2020-09-20 00:40:23 -0400

    need to pull

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:flutter/foundation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'backdrop.dart';
 
@@ -13,48 +12,6 @@ class HomePage extends StatelessWidget {
 
     return Backdrop(
       frontLayer: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 280,
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
-              child: Text('Welcome to BridgeGap!',
-                  style: Theme.of(context).textTheme.display1,
-                  textAlign: TextAlign.center),
-            ),
-            // Text(
-            //   "Welcome to BridgeGap!",
-            //   style: Theme.of(context).textTheme.display1,
-            //   textAlign: TextAlign.center,
-            // ),
-            SizedBox(
-              height: 40,
-            ),
-
-            Padding(
-              padding: EdgeInsets.fromLTRB(90,0,90,0),
-              child:RaisedButton(
-                color: Colors.green,
-                child: Text(
-                  "Get Matched",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  var arr = [1, 2, 3, 4, 5];
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Matches()),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
         // CustomScrollView(
 //          slivers: <Widget>[
 //            SliverAppBar(
@@ -74,7 +31,86 @@ class HomePage extends StatelessWidget {
 //          ],
 //        ),
 //    ============================================================================
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 280,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
+                child: Text('Welcome to BridgeGap!',
+                    style: Theme.of(context).textTheme.display1,
+                    textAlign: TextAlign.center),
+              ),
+              // Text(
+              //   "Welcome to BridgeGap!",
+              //   style: Theme.of(context).textTheme.display1,
+              //   textAlign: TextAlign.center,
+              // ),
+              SizedBox(
+                height: 40,
+              ),
 
+              Padding(
+                padding: EdgeInsets.fromLTRB(90,0,90,0),
+                child:RaisedButton(
+                  color: Colors.green,
+                  child: Text(
+                    "Get Matched",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Matches()),
+                    );
+                  },
+                ),
+              ),
+              // RaisedButton(
+              //   color: Colors.green,
+              //   child: Text(
+              //     "Get Matched",
+              //     style: TextStyle(color: Colors.white),
+              //   ),
+              //   onPressed: () {
+              //     Navigator.push(
+              //       context,
+              //       MaterialPageRoute(builder: (context) => Matches()),
+              //     );
+              //   },
+              // ),
+              SizedBox(
+                height: 800,
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "What's new:",
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      "No new updates to show.",
+                      style: Theme.of(context).textTheme.body1,
+                    )
+                  ],
+                ),
+                color: Colors.blueGrey[200],
+              ),
+              SizedBox(
+                height: 800,
+              )
+            ],
+          ),
+        ),
       ),
       backLayer: Container(
         color: Colors.blueGrey[100],
@@ -85,38 +121,27 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class MatchPairs {
-  MatchPairs(arr){
-    for (int i = arr.length; i>0; i--) {
-      print (i);
-    }
-
-  }
-}
-
 class Matches extends StatefulWidget {
   @override
   _MatchesState createState() => _MatchesState();
 }
 
 class _MatchesState extends State<Matches> {
+  //String profile;
   @override
   Widget build(BuildContext context){
     return Stack(
       alignment: Alignment.center,
       children: <Widget>[
         Card(
-          child: Container(
-            height: 600,
-            width: 350,
-            /*child: SizedBox(
-              height: 500,
-              child: Text(
+            child: Container(
+              height: 600,
+              width: 350,
+              child: SizedBox(
+                height: 100,
 
               ),
-
-            ),*/
-          )
+            )
         )
       ],
     );
